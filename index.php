32a7b47d95a469c75e83fe6e323f67cd3fe22f4b
Scodia619 <billyjoe2701@gmail.com>
2021-06-10 07:25:08 +0100

    Login Page php and CSS
    To start off the project ibe just recreated the Login page for the project where everyone will login or create a user.

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php ?>
+<html>
+
+<head>
+    <title>Babysitting Booking Program</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+    <form class="login">
+        <div class="container">
+            <div class="form-group">
+                <label for="exampleInputEmail1">Email address:</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+            </div>
+            <div class="form-group">
+                <label for="exampleInputPassword1">Password:</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+            </div>
+            <button type="submit" class="btn btn-primary">Login</button>
+            <hr  size="5" width="90%" color="black">
+            <button type="submit" class="btn btn-primary">Create A New User</button>
+        </div>
+    </form>
+</body>
+
+</html>
